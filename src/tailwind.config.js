6960032e48f7753ffb3d0ad5101963b4e5e1b03c
wendyhamel <whamel@it4web.nl>
2023-09-11 14:21:36 +0200

    working on responsive styling

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -5,8 +5,15 @@ tailwind.config = {
 		},
 		extend: {
 			colors: {
-				'':'',
-			}
+				'main-gradient-start':'hsl(0, 0%, 100%)',
+				'main-gradient-end':'hsl(0, 100%, 98%)',
+				'gradient-start':'hsl(0, 80%, 86%)',
+				'gradient-end':'hsl(0, 74%, 74%)',
+				'red-des':'hsl(0,36%,70%)',
+				'red-soft':'hsl(0,93%,70%)',
+				'red-dark':'hsl(0,6%,24%)',
+			},
+
 		}
 	}
 }
